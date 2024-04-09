f423765384aef9b11a26e4285af0d295dba58f4e
domalouf <89606118+domalouf@users.noreply.github.com>
2024-04-09 17:32:13 -0600

    Added picker to add sleep popup

--- src/screens/sleep/Sleep-Screen.js
+++ src/screens/sleep/Sleep-Screen.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { SafeAreaView, StatusBar, Text, StyleSheet, ScrollView, View } from 'react-native';
+import React, { useState } from 'react';
+import { Modal, SafeAreaView, StatusBar, Text, StyleSheet, ScrollView, View, TouchableOpacity, TextInput } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 const sleepData = [
   { day: 'March 1, 2024', hours: 7.5, quality: 'Good' },
@@ -14,33 +15,97 @@ const sleepData = [
   { day: 'March 10, 2024', hours: 6.5, quality: 'Poor' },
 ];
 
-// Individual day sleep report tab component
-const SleepTab = ({ dayReport }) => (
-  <View style={styles.sleepTab}>
-    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-      <View>
-        <Text style={styles.dateName}>{dayReport.day}</Text>
-        <Text style={styles.hoursText}>{`Hours of Sleep: ${dayReport.hours}`}</Text>
-      </View>
-      <View style={[styles.qualityCircle,
-      {
-        backgroundColor: dayReport.quality === 'Poor' ? 'red'
-          : dayReport.quality === 'Good' ? 'blue' : 'green'
-      }]}>
-        <Text style={styles.qualityText}>{dayReport.quality}</Text>
+const SleepScreen = (props) => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const AddSleepPopup = () => {
+    return (
+      <Modal
+        visible={isPopupVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setIsPopupVisible(!isPopupVisible)}
+      >
+        <View style={styles.popupOverlay}>
+          <View style={styles.popup}>
+            <View style={styles.popupHeader}>
+              <TouchableOpacity onPress={() => setIsPopupVisible(false)}>
+                <Text style={[styles.closeButton, { alignSelf: 'flex-start', fontSize: 24 }]}>x</Text>
+              </TouchableOpacity>
+
+              <Text style={styles.popupTitle}>New Sleep Data</Text>
+
+              <TouchableOpacity onPress={() => { /* Handle edit */ }}>
+                <Text style={styles.editButton}>Edit</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.popupContent}>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.addSleepInputText}>Hours Slept: </Text>
+                <TextInput placeholder="Enter hours" />
+              </View>
+
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text style={styles.addSleepInputText}>Quality: </Text>
+                <RNPickerSelect
+                  style={pickerSelectStyles}
+                  placeholder={{ label: "Select sleep quality", value: null }}
+                  onValueChange={(value) => console.log(value)}
+                  items={[
+                    { label: "Poor", value: "poor" },
+                    { label: "Good", value: "good" },
+                    { label: "Great", value: "great" },
+                  ]}
+                />
+              </View>
+
+              <TouchableOpacity
+                style={[styles.addSleepButton, { marginTop: 20 }]}
+                onPress={() => setIsPopupVisible(false)}>
+                <Text style={styles.addSleepButtonText}>Submit</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+
+  const SleepTab = ({ dayReport }) => (
+    <View style={styles.sleepTab}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View>
+          <Text style={styles.dateName}>{dayReport.day}</Text>
+          <Text style={styles.hoursText}>{`Hours of Sleep: ${dayReport.hours}`}</Text>
+        </View>
+        <View style={[styles.qualityCircle,
+        {
+          backgroundColor: dayReport.quality === 'Poor' ? 'red'
+            : dayReport.quality === 'Good' ? 'blue' : 'green'
+        }]}>
+          <Text style={styles.qualityText}>{dayReport.quality}</Text>
+        </View>
       </View>
     </View>
-  </View>
-)
+  )
 
-const SleepScreen = (props) => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
+        <AddSleepPopup />
         <Text style={styles.title}>Sleep History</Text>
 
-        <Text style={styles.monthText}>March 2024</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 20 }}>
+          <Text style={styles.monthText}>March 2024</Text>
+          <TouchableOpacity
+            style={styles.addSleepButton}
+            onPress={() => setIsPopupVisible(true)}>
+            <Text style={styles.addSleepButtonText}>+ Add Sleep</Text>
+          </TouchableOpacity>
+        </View>
 
         <ScrollView style={styles.sleepScrollContainer}>
           {sleepData.map((day, index) => (
@@ -70,6 +135,19 @@ const styles = StyleSheet.create({
     color: 'black',
     textAlign: 'left',
     marginTop: 10,
+  },
+  addSleepButton: {
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    padding: 10,
+    margin: 10,
+    justifyContent: 'center',
+    height: 50,
+  },
+  addSleepButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   sleepScrollContainer: {
     marginHorizontal: 20,
@@ -109,4 +187,60 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
   },
+  popupOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  // styles for the popup
+  popup: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 20,
+    width: '90%',
+  },
+  popupHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  popupTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  editButton: {
+    fontSize: 18,
+    color: '#0000ff', // Replace with your theme color
+  },
+  popupContent: {
+    marginVertical: 20,
+  },
+  addSleepInputText: {
+    fontSize: 18,
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 4,
+    color: 'blue',
+    paddingRight: 30 // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'blue',
+    borderRadius: 8,
+    color: 'blue',
+    paddingRight: 30 // to ensure the text is never behind the icon
+  }
 });
