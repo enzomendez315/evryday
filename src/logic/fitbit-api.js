de32208857050f6f0ffc5a77de13c12d20f67965
Enzo Mendez <enzo.mendez315@gmail.com>
2024-08-07 14:48:36 -0600

    Finished authentication class.

--- src/logic/fitbit-api.js
+++ src/logic/fitbit-api.js
@@ -65,9 +65,92 @@ class FitbitOAuth2Client {
         }
     }
 
+    // Makes requests to the specified url
+    // Only the url is required
+    // Requests will default to GET if method is not specified
+    async makeRequest(url, method = 'GET', data = null) {
+        try {
+            const response = await this.axiosInstance.request({
+                url: url,
+                method: method,
+                data: data,
+            });
+
+            return response.data;
+        } catch (error) { 
+            // Refresh access token if status is "Unauthorized"
+            if (error.response && error.response.status === 401) {
+                await this.refreshAccessToken();
+                return this.makeRequest(url, method, data);
+            } else {
+                throw error;
+            }
+        }
+    }
+
+    // Step 1
+    // Returns the url the user needs to go to in order to grant authorization
+    getAuthorizationUri(scope = null) {
+        // Use the defaults if no scope is provided
+        this.scope = scope || [
+            "activity",
+            "heartrate",
+            "location",
+            "nutrition",
+            "profile",
+            "settings",
+            "sleep",
+            "social",
+            "weight",
+        ];
+        
+        const authorizationUri = this.oAuthClient.authorizeURL({
+            redirect_uri: this.redirectUri,
+            scope: this.scope.join(' '), // Converted to a single string separated by spaces
+        });
+
+        return authorizationUri;
+    }
+
+    // Step 2
+    // Uses the code from step 1 and calls Fitbit again to get an access token object
+    async getAccessToken(code) {
+        const tokenParams = {
+            code: code,
+            redirect_uri: this.redirectUri,
+            scope: this.scope.join(' '),
+            //scope: 'activity heartrate location nutrition profile settings sleep social weight',
+        };
+
+        try {
+            const accessToken = await this.oAuthClient.getToken(tokenParams);
+            this.token = accessToken.token;
+            this.setAxiosHeaders();
+            return accessToken.token;
+        } catch (error) {
+            console.error('Access token error: ${error.message}');
+        }
+    }
+
+    // Step 3
+    // Obtains a new access token from the refresh token obtained in step 2
     async refreshAccessToken() {
         if (!this.token.refresh_token) {
             throw new Error("No refresh token available");
+        }
+
+        const tokenObject = this.oAuthClient.createToken(this.token);
+
+        try {
+            const refreshedToken = await tokenObject.refresh();
+            this.token = refreshedToken.token;
+            this.setAxiosHeaders();
+
+            if (this.refreshCallback) {
+                this.refreshCallback(this.token);
+            }
+        } catch (error) {
+            console.error('Error refreshing access token: ${error.message}');
         }
     }
 }
